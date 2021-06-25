560d204ff757f0705ded43cd33c169b18de7b09f
emoncse53 <emoncse53@gmail.com>
2021-06-25 14:20:05 +0600

    Added code to set product list page as root router using Switch from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import { Button } from 'react-bootstrap'
-import { BrowserRouter,Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Login from './Login'
 import Register from './Register'
 import AddProduct from './AddProduct'
 import UpdateProduct from './UpdateProduct'
 import Protected from './Protected'
+import ProductList from './ProductList';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/add">
-          <Protected Cmp={AddProduct} />
-        </Route>
-        <Route path="/update">
-          <Protected Cmp={UpdateProduct} />
-        </Route>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/add">
+            <Protected Cmp={AddProduct} />
+          </Route>
+          <Route path="/update">
+            <Protected Cmp={UpdateProduct} />
+          </Route>
+          <Route path="/">
+            <Protected Cmp={ProductList} />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
